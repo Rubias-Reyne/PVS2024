e637e762981eeba28529a3375f23862fc4e740a7
vladimir.topolcany <ag.rubias@gmail.com>
2024-09-09 12:47:17 +0200

    Create repository

---
+++ src/III/fileBasics.java
@@ -0,0 +1,9 @@
+package III;
+
+import fileworks.DataExport;
+
+public class fileBasics {
+    public static void main(String[] args) {
+        DataExport de = new DataExport("output.txt");
+    }
+}
