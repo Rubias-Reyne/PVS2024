3d596e9ba745c8cdaacda4d951f3cfa17e960aa2
vladimir.topolcany <ag.rubias@gmail.com>
2024-09-19 10:00:55 +0200

    4PRG - Stack/Heap

--- src/reference/CountDown.java
+++ src/reference/CountDown.java
@@ -4,22 +4,46 @@ class CountDown {
     int x;
 
     public static void main(String[] args) {
-        CountDown mc1 = new CountDown();
-		CountDown mc2 = new CountDown();
-		mc1.x = 1;
-		mc2.x = 2;
-		CountDown mc3 = mc2;
-		mc1.x += 3;
-		mc2.x += 4;
-		mc3.x += 5;
-		mc3.x = mc1.x;
-		mc3 = mc2;
-		mc2 = mc2;
-		mc1.x += 6;
-		mc2.x += 7;
-		mc3.x += 8;
-		System.out.println(mc1.x);
-		System.out.println(mc2.x);
-		System.out.println(mc3.x);
+//        CountDown mc1 = new CountDown();
+//		CountDown mc2 = new CountDown();
+//		CountDown mc3 = new CountDown();
+//		mc1.x = 1;
+//		mc2.x = 2;
+//		CountDown t = mc1;
+//		mc1.x += 3;
+//		mc2.x += 4;
+//		mc3.x += t.x;
+//		mc2 = mc1;
+
+
+
+//		mc1.x = 1;
+//		mc2.x = 2;
+//		CountDown mc3 = mc2;
+//		mc1.x += 3;
+//		mc2.x += 4;
+//		mc3.x += 5;
+
+//		mc3 = mc1;
+//		mc3.x = mc4.x;
+//		mc3 = mc2;
+
+//		mc2 = mc2;
+//		mc1.x += 6;
+//		mc2.x += 7;
+//		mc3.x += 8;
+//		System.out.println(mc1.x);
+//		System.out.println(mc2.x);
+//		System.out.println(mc3.x);
+
+//        int i1 = 3;
+//        int i2 = 4;
+//        int i3 = 5;
+//        int[] ii = {i1; i2; i3};
+//        i1 = i2;
+//        i1 ++;
+//        i2 + 3;
+//        ii[1] = 8;
+
     }
 }
---
+++ src/reference/ParsePerson.java
@@ -0,0 +1,46 @@
+package reference;
+
+import fileworks.DataImport;
+
+public class ParsePerson {
+    String fullName;
+    int Height;
+    int Weight;
+    int age;
+
+
+
+
+    public static void main(String[] args) {
+        DataImport di = new DataImport("osoba.txt");
+        String line;
+        String[] atributes;
+        ParsePerson p;
+        while(di.hasNext()){
+            line = di.readLine();
+            atributes = line.split(",");
+            p = new ParsePerson(atributes[0],
+                    Integer.parseInt(atributes[1]),
+                    Integer.parseInt(atributes[2]),
+                    Integer.parseInt(atributes[3]));
+        }
+        di.finishImport();
+    }
+
+    public ParsePerson(String fullName, int height, int weight, int age) {
+        this.fullName = fullName;
+        Height = height;
+        Weight = weight;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "ParsePerson{" +
+                "fullName='" + fullName + '\'' +
+                ", Height=" + Height +
+                ", Weight=" + Weight +
+                ", age=" + age +
+                '}';
+    }
+}
